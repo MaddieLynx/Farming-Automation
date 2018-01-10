0b8a291771d6b95880b157d21d106f48f782698d
isaacolson <olsonites@gmail.com>
2018-01-09 20:04:48 -0600

    Update pin_reader.py

--- Farmware/pin-reader/pin_reader.py
+++ Farmware/pin-reader/pin_reader.py
@@ -6,24 +6,24 @@ import os
 import json
 import requests
 
-def get_env(key, type_=int):
-    'Return the value of the namespaced Farmware input variable.'
-    return type_(os.environ['{}_{}'.format(farmware_name, key)])
+#def get_env(key, type_=int):
+#    'Return the value of the namespaced Farmware input variable.'
+#    return type_(os.environ['{}_{}'.format(farmware_name, key)])
 
-def set_servo_angle(pin, angle):
-    'Wrap the data in a `set_servo_angle` Celery Script command to send it.'
-    return {
-        "kind": "set_servo_angle",
-        "args": {"pin_number": pin, "pin_value": angle}}
+#def set_servo_angle(pin, angle):
+#    'Wrap the data in a `set_servo_angle` Celery Script command to send it.'
+#    return {
+#        "kind": "set_servo_angle",
+#        "args": {"pin_number": pin, "pin_value": angle}}
 
-def post(wrapped_data):
-    'Send the Celery Script command.'
-    headers = {
-        'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-        'content-type': "application/json"}
-    payload = json.dumps(wrapped_data)
-    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
-                  data=payload, headers=headers)
+#def post(wrapped_data):
+#    'Send the Celery Script command.'
+#    headers = {
+#        'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+#        'content-type': "application/json"}
+#    payload = json.dumps(wrapped_data)
+#    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+#                  data=payload, headers=headers)
 
 #if __name__ == "__main__":
 #    farmware_name = 'set_servo_angle'
